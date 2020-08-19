d11ccb093638ca61a9f5e9277ee5432ce67dbf5a
angonch <angonch@cs.washington.edu>
2020-08-19 15:53:19 -0700

    -basic UI -calculate tip when text field changed

--- TippySwift/ViewController.swift
+++ TippySwift/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSegmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        print("keyboard dismiss");
+        self.view.endEditing(true);
+    }
+    
+    @IBAction func onEdit(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0.0
+       
+        let percentages = [0.15, 0.20, 0.22]
+        let tipPercentage = Double(percentages[tipSegmentControl.selectedSegmentIndex])
+        
+        let tip = tipPercentage * bill
+        let total = bill + tip
+        
+        tipLabel.text = String.init(format: "$%.2f", tip)
+        totalLabel.text = String.init(format: "$%.2f", total)
+    }
 }
 
